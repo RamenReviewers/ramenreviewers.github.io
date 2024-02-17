012a6c8265715a108a817cd038ebbec897f00a8b
DESKTOP\Yannic <yapeulet@gmx.de>
2024-02-17 15:38:21 +0100

    Refactor Some test names and outsource helper methods

--- src/test/java/com/ramenreviewers/blog/ReviewYmlParserTest.java
+++ src/test/java/com/ramenreviewers/blog/ReviewYmlParserTest.java
@@ -6,7 +6,6 @@ import org.junit.jupiter.api.Test;
 
 import java.nio.file.Path;
 import java.nio.file.Paths;
-import java.util.List;
 import java.util.Objects;
 
 import static org.junit.jupiter.api.Assertions.*;
@@ -15,43 +14,17 @@ class ReviewYmlParserTest {
 
     @Test
     @SneakyThrows
-    void parseValidReview() {
+    void shouldParseValidReview() {
         Path reviewDirectory = Paths.get(Objects.requireNonNull(BlogApplication.class.getClassLoader().getResource("validReview")).toURI());
         Review result = ReviewYmlParser.parseReview(reviewDirectory);
-        Review expectedReview = getValidReview();
+        Review expectedReview = TestUtil.getValidReview();
 
         assertEquals(expectedReview, result);
     }
 
-    private static Review getValidReview() {
-        Review expectedReview = new Review();
-        expectedReview.setId("validReview");
-        expectedReview.setShop("TestShop");
-        expectedReview.setDish("TestDish");
-        expectedReview.setReviewers(List.of("Test1", "Test2"));
-        expectedReview.setPrice(15f);
-        expectedReview.setScoreBroth(5f);
-        expectedReview.setScoreNoodles(3.5f);
-        expectedReview.setScoreToppings(4f);
-        expectedReview.setScoreAtmosphere(2f);
-        expectedReview.setLocation("TestLocation");
-        expectedReview.setTags(List.of("TEST_TAG", "ANOTHER_TAG"));
-        expectedReview.setDescription("TEST DESCRIPTION");
-        expectedReview.setPicturePaths(
-                List.of(Paths.get("src", "main", "resources", "reviews", "validReview","thumbnail.jpg").toString(),
-                        Paths.get("src", "main", "resources", "reviews", "validReview","thumbnail.png").toString()
-                )
-        );
-        var link = new Review.Link();
-        link.setDisplayName("test");
-        link.setUrl("https://test.com/");
-        expectedReview.setLinks(List.of(link));
-        return expectedReview;
-    }
-
     @Test
     @SneakyThrows
-    void parseReviewWithInvalidYAML() {
+    void parsingReviewWithInvalidYAMLShouldThrowException() {
         Path reviewDirectory = Paths.get(Objects.requireNonNull(BlogApplication.class.getClassLoader().getResource("invalidReview")).toURI());
         assertThrows(Exception.class, () -> ReviewYmlParser.parseReview(reviewDirectory));
 
@@ -59,45 +32,26 @@ class ReviewYmlParserTest {
 
     @Test
     @SneakyThrows
-    void parseReviewWithMissingFile() {
+    void parsingReviewWithMissingFileShouldThrowException() {
         Path reviewDirectory = Paths.get(Objects.requireNonNull(BlogApplication.class.getClassLoader().getResource("missingReview")).toURI());
         assertThrows(RuntimeException.class, ()-> ReviewYmlParser.parseReview(reviewDirectory));
     }
 
     @Test
     @SneakyThrows
-    void parseReviewWithoutImage() {
+    void shouldParseValidReviewWithoutImage() {
         Path reviewDirectory = Paths.get(Objects.requireNonNull(BlogApplication.class.getClassLoader().getResource("validReviewNoImage")).toURI());
         Review result = ReviewYmlParser.parseReview(reviewDirectory);
-        Review expectedReview = getNoImageReview();
+        Review expectedReview = TestUtil.getValidNoImageReview();
 
         assertEquals(expectedReview, result);
     }
 
-    private static Review getNoImageReview() {
-        Review expectedReview = new Review();
-        expectedReview.setId("validReviewNoImage");
-        expectedReview.setShop("TestShop");
-        expectedReview.setDish("TestDish");
-        expectedReview.setReviewers(List.of("Test1", "Test2"));
-        expectedReview.setPrice(15f);
-        expectedReview.setScoreBroth(5f);
-        expectedReview.setScoreNoodles(3.5f);
-        expectedReview.setScoreToppings(4f);
-        expectedReview.setScoreAtmosphere(2f);
-        expectedReview.setLocation("TestLocation");
-        expectedReview.setTags(List.of());
-        expectedReview.setDescription("");
-        var link = new Review.Link();
-        link.setDisplayName("test");
-        link.setUrl("https://test.com/");
-        expectedReview.setLinks(List.of(link));
-        return expectedReview;
-    }
+
 
     @Test
     @SneakyThrows
-    void testTooLongDescription() {
+    void parsingTooLongDescriptionShouldThrowException() {
         Path reviewDirectory = Paths.get(Objects.requireNonNull(BlogApplication.class.getClassLoader().getResource("longDescriptionReview")).toURI());
         assertThrows(Exception.class, () -> ReviewYmlParser.parseReview(reviewDirectory));
     }
