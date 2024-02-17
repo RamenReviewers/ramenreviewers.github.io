ff5c8724b6e9a05934796b45d1cc5f7813c5dd56
DESKTOP\Yannic <yapeulet@gmx.de>
2024-02-17 19:08:49 +0100

    Remove always failing Test

--- src/test/java/com/ramenreviewers/blog/ReviewTemplateProcessorTest.java
+++ src/test/java/com/ramenreviewers/blog/ReviewTemplateProcessorTest.java
@@ -4,18 +4,11 @@ import org.junit.jupiter.api.Test;
 
 import java.util.List;
 
-import static org.junit.jupiter.api.Assertions.fail;
-
 class ReviewTemplateProcessorTest {
 
     @Test
     void shouldProcessListOfValidReviews(){
         var reviews = List.of(Helper.getValidReview());
         ReviewTemplateProcessor.processTemplate(reviews);
-    }
-
-    @Test
-    void willFail(){
-        fail();
     }
 }
