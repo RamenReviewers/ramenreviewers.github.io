c586fb1e9a798563199a3b7b84d77e9975889032
DESKTOP\Yannic <yapeulet@gmx.de>
2024-02-17 18:25:00 +0100

    Will Be reverted, just checking

--- src/test/java/com/ramenreviewers/blog/ReviewTemplateProcessorTest.java
+++ src/test/java/com/ramenreviewers/blog/ReviewTemplateProcessorTest.java
@@ -4,11 +4,19 @@ import org.junit.jupiter.api.Test;
 
 import java.util.List;
 
+import static org.junit.jupiter.api.Assertions.assertTrue;
+import static org.junit.jupiter.api.Assertions.fail;
+
 class ReviewTemplateProcessorTest {
 
     @Test
     void shouldProcessListOfValidReviews(){
         var reviews = List.of(TestUtil.getValidReview());
         ReviewTemplateProcessor.processTemplate(reviews);
+    }
+
+    @Test
+    void willFail(){
+        fail();
     }
 }
